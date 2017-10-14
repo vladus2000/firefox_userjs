b9b50432498f2aac617ba031a70c6a43ad51aef7
vladus2000 <github@matt.land>
2017-10-14 11:19:55 -0500

    first commit

---
+++ user.js
@@ -0,0 +1,137 @@
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("beacon.enabled", false);
+user_pref("browser.fixup.alternate.enabled", false);
+user_pref("browser.newtab.preload", false);
+user_pref("browser.newtabpage.enhanced", false);
+user_pref("browser.onboarding.shieldstudy.enabled", false);
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("devtools.theme", "dark");
+user_pref("experiments.enabled", false);
+user_pref("experiments.supported", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("media.autoplay.enabled", false);
+user_pref("media.getusermedia.screensharing.enabled", false);
+user_pref("media.navigator.enabled", false);
+user_pref("media.navigator.video.enabled", false);
+user_pref("media.peerconnection.enabled", false);
+user_pref("media.peerconnection.ice.default_address_only", true);
+user_pref("media.peerconnection.turn.disable", true);
+user_pref("media.peerconnection.video.enabled", false);
+user_pref("media.video_stats.enabled", false);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("noscript.sync.enabled", true);
+user_pref("plugin.default.state", 0);
+user_pref("plugin.state.flash", 0);
+user_pref("plugin.state.java", 0);
+user_pref("plugins.click_to_play", true);
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("social.remote-install.enabled", false);
+user_pref("social.share.activationPanelEnabled", false);
+user_pref("social.toast-notifications.enabled", false);
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.unified", false);
+
+/* 0101: disable "slow startup" options
+ * warnings, disk history, welcomes, intros, EULA, default browser check ***/
+user_pref("browser.slowStartup.notificationDisabled", true);
+user_pref("browser.slowStartup.maxSamples", 0);
+user_pref("browser.slowStartup.samples", 0);
+user_pref("browser.rights.3.shown", true);
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("startup.homepage_welcome_url", "");
+user_pref("startup.homepage_welcome_url.additional", "");
+user_pref("startup.homepage_override_url", ""); // what's new page after updates
+user_pref("browser.laterrun.enabled", false);
+//user_pref("browser.shell.checkDefaultBrowser", false);
+
+/* 0202: disable GeoIP-based search results
+ * [NOTE] May not be hidden if Firefox has changed your settings due to your locale
+ * [1] https://trac.torproject.org/projects/tor/ticket/16254
+ * [2] https://support.mozilla.org/en-US/kb/how-stop-firefox-making-automatic-connections#w_geolocation-for-default-search-engine ***/
+user_pref("browser.search.countryCode", "US"); // (hidden pref)
+user_pref("browser.search.region", "US"); // (hidden pref)
+user_pref("browser.search.geoip.url", "");
+/* 0203: disable using OS locale, force APP locale ***/
+user_pref("intl.locale.matchOS", false);
+/* 0204: set APP locale ***/
+user_pref("general.useragent.locale", "en-US");
+/* 0206: disable geographically specific results/search engines e.g. "browser.search.*.US"
+ * i.e. ignore all of Mozilla's various search engines in multiple locales ***/
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+/* 0207: set language to match ***/
+user_pref("intl.accept_languages", "en-US, en");
+/* 0208: enforce US English locale regardless of the system locale
+ * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=867501 ***/
+user_pref("javascript.use_us_english_locale", true); // (hidden pref)
+/* 0209: use APP locale over OS locale in regional preferences (FF56+)
+ * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1379420 [also 1364789] ***/
+user_pref("intl.regional_prefs.use_os_locales", false);
+/* 0210: use Mozilla geolocation service instead of Google when geolocation is enabled
+ * Optionally enable logging to the console (defaults to false) ***/
+user_pref("geo.wifi.uri", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+// user_pref("geo.wifi.logging.enabled", true); // (hidden pref)
+
+/* 0309: disable sending Flash crash reports ***/
+user_pref("dom.ipc.plugins.flash.subprocess.crashreporter.enabled", false);
+/* 0310: disable sending the URL of the website where a plugin crashed ***/
+user_pref("dom.ipc.plugins.reportCrashURL", false);
+/* 0320: disable about:addons' Get Add-ons panel (uses Google-Analytics) ***/
+user_pref("extensions.getAddons.showPane", false); // hidden pref
+user_pref("extensions.webservice.discoverURL", "");
+/* 0330: disable telemetry
+ * the pref (.unified) affects the behaviour of the pref (.enabled)
+ * IF unified=false then .enabled controls the telemetry module
+ * IF unified=true then .enabled ONLY controls whether to record extended data
+ * so make sure to have both set as false
+ * [1] https://firefox-source-docs.mozilla.org/toolkit/components/telemetry/telemetry/internals/preferences.html ***/
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.cachedClientID", "");
+user_pref("toolkit.telemetry.newProfilePing.enabled", false); // (FF55+)
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false); // (FF55+)
+user_pref("toolkit.telemetry.updatePing.enabled", false); // (FF56+)
+/* 0333a: disable health report ***/
+user_pref("datareporting.healthreport.uploadEnabled", false);
+/* 0333b: disable about:healthreport page (which connects to Mozilla for locale/css+js+json)
+ * If you have disabled health reports, then this about page is useless - disable it
+ * If you want to see what health data is present, then this must be set at default ***/
+user_pref("datareporting.healthreport.about.reportUrl", "data:text/plain,");
+/* 0334: disable new data submission, master kill switch (FF41+)
+ * If disabled, no policy is shown or upload takes place, ever
+ * [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1195552 ***/
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+/* 0350: disable crash reports ***/
+user_pref("breakpad.reportURL", "");
+/* 0351: disable sending of crash reports (FF44+) ***/
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.enabled", false); // (FF51+)
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false); // (FF51+)
+/* 0360: disable new tab tile ads & preload & marketing junk ***/
+user_pref("browser.newtab.preload", false);
+user_pref("browser.newtabpage.directory.source", "data:text/plain,");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtabpage.enhanced", false);
+user_pref("browser.newtabpage.introShown", true);
+/* 0370: disable "Snippets" (Mozilla content shown on about:home screen)
+ * MUST use HTTPS - arbitrary content injected into this page via http opens up MiTM attacks
+ * [1] https://wiki.mozilla.org/Firefox/Projects/Firefox_Start/Snippet_Service ***/
+user_pref("browser.aboutHomeSnippets.updateUrl", "https://127.0.0.1");
+/* 0374: disable "social" integration
+ * [1] https://developer.mozilla.org/docs/Mozilla/Projects/Social_API ***/
+user_pref("social.whitelist", "");
+user_pref("social.toast-notifications.enabled", false);
+user_pref("social.shareDirectory", "");
+user_pref("social.remote-install.enabled", false);
+user_pref("social.directories", "");
+user_pref("social.share.activationPanelEnabled", false);
+user_pref("social.enabled", false); // (hidden pref)
+
+
